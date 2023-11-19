0c19f6c4c9a9a9b235dfbf298787b0bbb2e91e5d
09NINE90 <80192771+09NINE90@users.noreply.github.com>
2023-11-19 14:08:00 +0500

    Add files via upload

---
+++ demo-spring-boot/src/main/java/com/raz/demoboot/services/ProductsService.java
@@ -0,0 +1,30 @@
+package com.raz.demoboot.services;
+
+import com.raz.demoboot.entities.Product;
+import com.raz.demoboot.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductsService {
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product getById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public void add(Product product) {
+        productRepository.save(product);
+    }
+}
